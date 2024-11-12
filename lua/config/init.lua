73b6498a94fbca5f413f5fdeebd11c281daafb2c
raising <newraisingray@gmail.com>
2024-11-12 11:16:50 +0300

    initial config

---
+++ lua/config/init.lua
@@ -0,0 +1,6 @@
+--requires here
+require("config/telescope")
+require("config/theme")
+require("config/treesitter")
+require("config/lsp")
+
---
+++ lua/plugins/lazy.lua
@@ -0,0 +1,37 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+require("lazy").setup({
+        {'nvim-telescope/telescope.nvim', tag = '0.1.8',dependencies = { 'nvim-lua/plenary.nvim' }},
+        {"folke/tokyonight.nvim",lazy = false,priority = 1000,opts = {},},
+        {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+        {'windwp/nvim-autopairs',event = "InsertEnter",config = true,},
+        {"neovim/nvim-lspconfig"},
+        {"williamboman/mason.nvim"},
+        {"williamboman/mason-lspconfig.nvim"},
+        {"hrsh7th/cmp-nvim-lsp"},
+        {"hrsh7th/cmp-buffer"},
+        {"hrsh7th/cmp-path"},
+        {"hrsh7th/cmp-cmdline"},
+        {"hrsh7th/nvim-cmp"},
+        {"L3MON4D3/LuaSnip"},
+        {"saadparwaiz1/cmp_luasnip"},
+        {"j-hui/fidget.nvim"},
+
+})
+
