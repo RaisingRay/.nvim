155d1824a2b4ab6cd07381bb59f0e50dc6cad0d5
raising <newraisingray@gmail.com>
2024-11-12 22:13:12 +0300

    partial fix for format

--- lua/config/format.lua
+++ lua/config/format.lua
@@ -3,18 +3,21 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettier.with({
-            extra_filetypes = { "javascriptreact", "typescriptreact" }, -- for React
-            extra_args = { "--single-quote", "--jsx-single-quote" }, -- optional Prettier args
+            extra_filetypes = { "javascriptreact", "typescriptreact" },
+            extra_args = { "--single-quote", "--jsx-single-quote" },
         }),
     },
     on_attach = function(client, bufnr)
         if client.server_capabilities.documentFormattingProvider then
-            vim.api.nvim_command [[augroup Format]]
-            vim.api.nvim_command [[autocmd! * <buffer>]]
-            vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
-            vim.api.nvim_command [[augroup END]]
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
         end
     end,
 })
-
 
